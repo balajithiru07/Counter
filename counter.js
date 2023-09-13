6cb741e58640adbb038766d6014d14a0ed24bf00
balajithiru07 <132432477+balajithiru07@users.noreply.github.com>
2023-09-13 20:35:27 +0530

    first commit

---
+++ counter.js
@@ -0,0 +1,19 @@
+const counterElement=document.getElementById('counter');
+const incrementElement=document.getElementById('increment');
+const decrementElement=document.getElementById('decrement');
+
+let counterValue=0;
+counterElement.textContent=counterValue;
+
+
+incrementElement.addEventListener("click",()=>{
+    counterValue++;
+    counterElement.textContent=counterValue;
+
+});
+
+decrementElement.addEventListener("click",()=>{
+    counterValue--;
+    counterElement.textContent=counterValue;
+
+});
